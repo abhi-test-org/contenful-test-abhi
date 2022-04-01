1718af1944a374c9bcf20e5e267afb246ab0fff9
Abhi Aiyer <abhiaiyer91@gmail.com>
2022-04-01 08:29:01 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,3 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 const { contentfulConfig } = require('./gatsby-shared')
 
 const { spaceId, accessToken } = contentfulConfig
